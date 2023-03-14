5342ab505e96745def45f285a5c0cbbd5add2c57
Thomas Mayrhofer <thomas@mayrhofer.at>
2023-03-14 11:15:33 +0100

    Revert "2023-03-13 bachelor"

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
-const TestComponent = () => {
-  return (
-    <div>
-      hallo
-    </div>
-  )
-}
-
 test('renders learn react link', () => {
-  render(<TestComponent />);
-  const linkElement = screen.getByText('hallo');
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
